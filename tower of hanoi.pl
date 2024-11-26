ba20c8bbe1a39d50501cd6f89347789fc8a1f4d5
Akash B <113533511+Akash-1006@users.noreply.github.com>
2024-11-27 05:23:57 +0530

    Update tower of hanoi.pl

--- tower of hanoi.pl
+++ tower of hanoi.pl
@@ -1,28 +1,22 @@
 hanoi(1, Source, _, Destination) :-
-
     write('Move top disk from '),
-    write(X),
+    write(Source),
     write(' to '),
-    write(Y),
+    write(Destination),
     nl.
 
 hanoi(N, Source, Auxiliary, Destination) :-
-
+    N > 1, % Ensure N is greater than 1 for this rule
     M is N - 1,
-    hanoi(M, Source, Destination, Auxiliary),
+    hanoi(M, Source, Destination, Auxiliary), % Move N-1 disks from Source to Auxiliary
+    hanoi(1, Source, _, Destination),        % Move the nth disk from Source to Destination
+    hanoi(M, Auxiliary, Source, Destination). % Move N-1 disks from Auxiliary to Destination
 
-    hanoi(1, Source, _, Destination),
-
-    hanoi(M, Auxiliary, Source, Destination).
-
-OUTPUT:
-?-hanoi(3).
-
-Move disk from A to C
-Move disk from A to B
-Move disk from C to B
-Move disk from A to C
-Move disk from B to A
-Move disk from B to C
-Move disk from A to C
-true 
+?- hanoi(3, left, middle, right).
+Move top disk from left to right
+Move top disk from left to middle
+Move top disk from right to middle
+Move top disk from left to right
+Move top disk from middle to left
+Move top disk from middle to right
+Move top disk from left to right
